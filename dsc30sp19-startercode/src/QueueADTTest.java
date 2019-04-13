e9f78b71d0919c614e089408a67ef197d39a877f
zhg061 <45829527+zhg061@users.noreply.github.com>
2019-04-13 13:31:54 -0700

    update starter code

---
+++ dsc30sp19-startercode/src/QueueADTTest.java
@@ -0,0 +1,174 @@
+import static org.junit.Assert.*;
+
+public class QueueADTTest {
+
+    @org.junit.Test
+    public void add() {
+        CircularArrayQueue t1 = new CircularArrayQueue();
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        assertArrayEquals(new int[] {1, 1, 1, 1, 1, 1, 0, 0, 0, 0}, t1.getter());
+        CircularArrayQueue t2 = new CircularArrayQueue();
+        t2.add(1);
+        t2.add(2);
+        t2.add(1);
+        t2.add(8);
+        t2.add(-1);
+        t2.remove();
+        assertArrayEquals(new int[] {2, 1, 8, -1, 0}, t2.getter());
+        CircularArrayQueue t3 = new CircularArrayQueue(2);
+        t3.add(1);
+        t3.add(2);
+        t3.add(8);
+        t3.remove();
+        t3.add(-1);
+        t3.remove();
+        assertArrayEquals(new int[] {8, -1, 0, 0}, t3.getter());
+    }
+
+    @org.junit.Test
+    public void isEmpty() {
+        CircularArrayQueue t1 = new CircularArrayQueue();
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        assertEquals(false, t1.isEmpty());
+        CircularArrayQueue t2 = new CircularArrayQueue();
+        t2.add(1);
+        t2.remove();
+        assertEquals(true, t2.isEmpty());
+        CircularArrayQueue t3 = new CircularArrayQueue(2);
+        t3.add(1);
+        t3.add(2);
+        t3.add(8);
+        t3.remove();
+        t3.add(-1);
+        t3.remove();
+        t3.clear();
+        assertEquals(true, t3.isEmpty());
+    }
+
+    @org.junit.Test
+    public void peek() {
+        CircularArrayQueue t1 = new CircularArrayQueue();
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        assertEquals(1, t1.peek());
+        CircularArrayQueue t2 = new CircularArrayQueue();
+        t2.add(1);
+        t2.add(2);
+        t2.add(1);
+        t2.add(8);
+        t2.add(-1);
+        t2.remove();
+        assertEquals(2, t2.peek());
+        CircularArrayQueue t3 = new CircularArrayQueue(2);
+        t3.add(1);
+        t3.add(2);
+        t3.add(8);
+        t3.remove();
+        t3.add(-1);
+        t3.remove();
+        assertEquals(8, t3.peek());
+    }
+
+    @org.junit.Test
+    public void remove() {
+        CircularArrayQueue t1 = new CircularArrayQueue();
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        assertArrayEquals(new int[]{1, 1, 1, 1, 1, 1, 0, 0, 0, 0}, t1.getter());
+        t1.remove();
+        assertEquals(5, t1.getter2());
+        assertArrayEquals(new int[]{1, 1, 1, 1, 1, 0, 0, 0, 0, 0}, t1.getter());
+        CircularArrayQueue t2 = new CircularArrayQueue();
+        t2.add(1);
+        t2.add(2);
+        t2.add(1);
+        t2.add(8);
+        t2.add(-1);
+        assertEquals(1, t2.remove());
+        assertEquals(2, t2.remove());
+        assertEquals(1, t2.remove());
+        assertEquals(8, t2.remove());
+        assertArrayEquals(new int[] {-1, 0, 0, 0, 0}, t2.getter());
+        CircularArrayQueue t3 = new CircularArrayQueue(2);
+        t3.add(1);
+        t3.add(2);
+        t3.add(8);
+        t3.remove();
+        t3.add(-1);
+        t3.remove();
+        t3.remove();
+        t3.remove();
+        assertArrayEquals(new int[] {0, 0, 0, 0}, t3.getter());
+    }
+
+    @org.junit.Test
+    public void clear() {
+        CircularArrayQueue t1 = new CircularArrayQueue();
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.clear();
+        assertArrayEquals(new int[10], t1.getter());
+        CircularArrayQueue t2 = new CircularArrayQueue(2);
+        t2.add(1);
+        t2.add(2);
+        t2.add(1);
+        t2.add(8);
+        t2.add(-1);
+        t2.clear();
+        assertArrayEquals(new int[8], t2.getter());
+        CircularArrayQueue t3 = new CircularArrayQueue(2);
+        t3.add(1);
+        t3.add(2);
+        t3.add(8);
+        t3.clear();
+        assertArrayEquals(new int[] {0, 0, 0, 0}, t3.getter());
+    }
+
+    @org.junit.Test
+    public void size() {
+        CircularArrayQueue t1 = new CircularArrayQueue();
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        t1.add(1);
+        assertEquals(6, t1.size());
+        CircularArrayQueue t2 = new CircularArrayQueue(2);
+        t2.add(1);
+        t2.add(2);
+        t2.add(1);
+        t2.add(8);
+        t2.add(-1);
+        t2.clear();
+        assertEquals(0, t2.size());
+        CircularArrayQueue t3 = new CircularArrayQueue(2);
+        t3.add(1);
+        t3.add(2);
+        t3.add(8);
+        t3.clear();
+        assertEquals(0, t3.size());
+    }
+}
---
+++ dsc30sp19-startercode/src/SunshineMarket.java
@@ -0,0 +1,82 @@
+/**
+ *  TODO: add class header
+ */
+public class SunshineMarket {
+    // Customer
+    private static int[] customers1 = {3, 7, 20};
+    private static int[] customers2 = {1, 3, 5, 4, 16, 8};
+    private static int[] customers3 = {1, 1, 2, 3, 5, 7};
+    
+    private static QueueADT customersQueue;
+    private static QueueADT[] registers;
+
+
+    
+    /**
+     * This is the program entry where we will run our simulation
+     *
+     * @param args commandline arguments
+     */
+    public static void main(String[] args) {
+        
+        SunshineMarket.timeInfo(customers1, 3);
+    }
+    
+    /**
+     * Returns a string that contains information about total time
+     * for checking out all customers and register idle time.
+     * @param customers the customer queue
+     * @param numberOfRegisters number of registers opened
+     * @return a string that contains information about total time
+     *         for checking out all customers and register idle time.
+     */
+//    change string to queaft
+    public static String timeInfo(int[] customers, int numberOfRegisters) {
+        int totalTime = 0;
+        int registersIdleTime = 0;
+        customersQueue = new CircularArrayQueue();
+        registers = new QueueADT[numberOfRegisters];
+        for (int i = 0; i < numberOfRegisters; i++) {
+            registers[i] = new CircularArrayQueue();
+        }
+        for (int j = 0; j < customers.length; j++) {
+            customersQueue.add(customers[j]);
+            QueueADT temporaryRegister = findFirstEmptyRegister();
+            for (int z = 0; z < customers[j]; z++) {
+                temporaryRegister.add(1);
+            }
+            customersQueue.remove();
+        }
+        System.out.println(registers.length);
+        return "With " + numberOfRegisters +
+        " lines, the total time for checking out all customers was "
+        + totalTime + " time units, and registers were idle for a total of "
+        + registersIdleTime + " time units.\n";
+    }
+    
+    /**
+     * Helper method to find the first empty register. Return null if all registers are not empty.
+     * @return the first empty register
+     */
+    private static QueueADT findFirstEmptyRegister() {
+        
+        for (int i = 0; i < registers.length; i++) {
+            if (registers[i].isEmpty()) {
+                return registers[i];
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Helper method to determine if the market is empty
+     *
+     * @return true if the whole market is empty
+     */
+    private static boolean marketIsEmpty() {
+        
+        // TODO
+        
+        return false;
+    }
+}
